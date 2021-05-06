312f2f7bdfd1bcb3e3c758db92ae221cbbb318f7
auzanassdq <auzanassidqi@gmail.com>
2021-05-06 23:08:57 +0700

    add request fail

--- src/redux/actions/matkulAction.js
+++ src/redux/actions/matkulAction.js
@@ -1,11 +1,18 @@
 import axios from 'axios'
 
 export const API_REQUEST = "API_REQUEST"
+export const REQUEST_FAIL = "REQUEST_FAIL"
 export const GET_MATKUL_SUCCESS = "GET_MATKUL_SUCCESS"
 export const GET_ALL_MATKUL_SUCCESS = "GET_ALL_MATKUL_SUCCESS"
 export const GET_MAHASISWA_MATKUL_SUCCESS = "GET_MAHASISWA_MATKUL_SUCCESS"
 
 const apiRequest = () => {
+  return {
+    type: API_REQUEST
+  }
+}
+
+const requestFail = () => {
   return {
     type: API_REQUEST
   }
@@ -42,15 +49,21 @@ export const getMatkulByID = (idMatkul) => async (dispatch) => {
       'Authorization': `Bearer ${token}`
     }
   })
-  dispatch(getMatkulSuccess(result.data.data))
+  if (result.data) {
+    dispatch(getMatkulSuccess(result.data.data))
+  } else {
+    dispatch(requestFail())
+  }
 }
 
+// belum kepake
 export const getAllMatkul = () => async (dispatch) => {
   dispatch(apiRequest())
   
   let result = await axios.get(process.env.REACT_APP_API + "/ambil-matkul")
   console.log(result);
   dispatch(getAllMatkulSuccess(result.data.data))
+
 }
 
 export const getMahasiswaMatkul = (userId) => async (dispatch) => {
@@ -64,5 +77,9 @@ export const getMahasiswaMatkul = (userId) => async (dispatch) => {
         'Authorization': `Bearer ${token}`
       }
     })
-  dispatch(getMahasiswaMatkulSuccess(result.data.data))
+  if(result.data){
+    dispatch(getMahasiswaMatkulSuccess(result.data.data))
+  } else {
+    dispatch(requestFail())
+  }
 }
--- src/redux/reducers/matkulReducer.js
+++ src/redux/reducers/matkulReducer.js
@@ -1,16 +1,17 @@
 import {
   API_REQUEST,
+  REQUEST_FAIL,
   GET_MATKUL_SUCCESS,
   GET_ALL_MATKUL_SUCCESS,
-  GET_MAHASISWA_MATKUL_SUCCESS
-} from '../actions/matkulAction'
+  GET_MAHASISWA_MATKUL_SUCCESS,
+} from '../actions/matkulAction';
 
 const initialState = {
   isLoading: false,
   allMatkul: [],
   mahasiswaMatkul: [],
-  matkul: {}
-}
+  matkul: {},
+};
 
 const matkulReducer = (state = initialState, action) => {
   switch (action.type) {
@@ -18,28 +19,33 @@ const matkulReducer = (state = initialState, action) => {
       return {
         ...state,
         isLoading: true,
-      }
+      };
+    case REQUEST_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+      };
     case GET_MATKUL_SUCCESS:
       return {
         ...state,
         isLoading: false,
         matkul: action.payload,
-      }
+      };
     case GET_ALL_MATKUL_SUCCESS:
       return {
         ...state,
         isLoading: false,
         allMatkul: action.payload,
-      }
+      };
     case GET_MAHASISWA_MATKUL_SUCCESS:
       return {
         ...state,
         isLoading: false,
-        mahasiswaMatkul: action.payload
-      }
+        mahasiswaMatkul: action.payload,
+      };
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default matkulReducer
+export default matkulReducer;
