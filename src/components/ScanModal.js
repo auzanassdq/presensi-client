d29e574e01b3ee7ffdf09c30947d135110655e03
auzanassdq <auzanassidqi@gmail.com>
2021-05-07 23:46:18 +0700

    fix handle submit

--- src/components/ScanModal.js
+++ src/components/ScanModal.js
@@ -1,19 +1,41 @@
 import { Button } from '@chakra-ui/button'
 import { ModalCloseButton,ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Modal } from '@chakra-ui/modal'
-import axios from 'axios'
-import React from 'react'
+import { useToast } from '@chakra-ui/toast'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { checkInKehadiran, getKehadiranSuccess } from '../redux/actions/kehadiranAction'
 
-export default function ScanModal({isOpen, onClose}) {
+export default function ScanModal({isOpen, onClose, pertemuan}) {
+  const dispatch = useDispatch()
+  const toast = useToast()
+  const {userId} = useSelector(state => state.userReducer)
+  const [dataCheckIn] = useState({mahasiswa: userId, pertemuan});
 
   const handleSubmit = () => {
-    // axios.put
+    dispatch(checkInKehadiran(dataCheckIn))
+    .then(result => {
+      dispatch(getKehadiranSuccess(result))
+      toast({
+        title: "Berhasil CheckIn",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      })
+    }).catch(error => {
+      toast({
+        title: "Gagal CheckIn",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      })
+    })
   }
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Pemindai Wajah</ModalHeader>
+        <ModalHeader>{pertemuan.nama}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           TEss
