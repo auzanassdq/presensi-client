c214dcd9494802fe12d0bb81a38316535ba92b3e
auzanassdq <auzanassidqi@gmail.com>
2021-05-07 23:36:32 +0700

    create kehadiran reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux'
 import userReducer from './userReducer'
 import matkulReducer from './matkulReducer'
 import pertemuanReducer from './pertemuanReducer'
+import kehadiranReducer from './kehadiranReducer'
 
 
 const rootReducer = combineReducers({
   userReducer,
   matkulReducer,
   pertemuanReducer,
+  kehadiranReducer
 });
 
 export default rootReducer;
