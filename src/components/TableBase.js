9034b2b8fd43b3e8bf9fdb989ee5d007a5e2cf13
auzanassdq <auzanassidqi@gmail.com>
2021-05-09 00:39:53 +0700

    add key every return in map, and add more styling

--- src/components/TableBase.js
+++ src/components/TableBase.js
@@ -2,8 +2,14 @@ import { useTable, useSortBy } from 'react-table';
 import React from 'react';
 import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/table';
 import { Button } from '@chakra-ui/button';
+import { useColorModeValue } from '@chakra-ui/color-mode';
+import { useHistory, useRouteMatch } from 'react-router';
 
 export default function TableBase({ columns, data }) {
+  const thColor = useColorModeValue('blackAlpha.400', 'blackAlpha.700');
+  const history = useHistory();
+  const { path, url } = useRouteMatch();
+  console.log(path, url);
   const {
     getTableProps,
     getTableBodyProps,
@@ -17,19 +23,23 @@ export default function TableBase({ columns, data }) {
     },
     useSortBy
   );
-  console.log(headers);
+
   const firstPageRows = rows.slice(0, 20);
 
   const lihatPertemuan = matkul => {
     console.log(matkul);
+    history.push(`${url}/${matkul._id}`);
   };
 
   return (
     <Table variant="striped" colorScheme="blue" size="sm" {...getTableProps()}>
       <Thead>
-        <Tr>
-          {headers.map(column => (
-            <Th p="1" {...column.getHeaderProps(column.getSortByToggleProps())}>
+        <Tr bg={thColor}>
+          {headers.map((column, index) => (
+            <Th
+              key={index}
+              {...column.getHeaderProps(column.getSortByToggleProps())}
+            >
               {column.render('Header')}
               {column.isSorted ? (column.isSortedDesc ? ' ∨' : ' ∧') : ''}
             </Th>
@@ -37,26 +47,31 @@ export default function TableBase({ columns, data }) {
         </Tr>
       </Thead>
       <Tbody {...getTableBodyProps()}>
-        {firstPageRows.map((row, i) => {
+        {firstPageRows.map((row, index) => {
           prepareRow(row);
           return (
-            <Tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
+            <Tr key={index} {...row.getRowProps()}>
+              {row.cells.map((cell, index) => {
+                // console.log(cell);
                 if (cell.column.Header === 'Opsi') {
                   return (
-                    <Td p="1">
+                    <Td key={index} p="1">
                       <Button
                         colorScheme="blue"
                         variant="outline"
                         size="xs"
-                        onClick={() => lihatPertemuan(cell.row.values)}
+                        onClick={() => lihatPertemuan(cell.row.original)}
                       >
                         Pertemuan
                       </Button>
                     </Td>
                   );
                 }
-                return <Td {...cell.getCellProps()} p="1" fontSize="xs">{cell.render('Cell')}</Td>;
+                return (
+                  <Td {...cell.getCellProps()} fontSize="xs">
+                    {cell.render('Cell')}
+                  </Td>
+                );
               })}
             </Tr>
           );
