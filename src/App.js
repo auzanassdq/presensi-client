bd58be718d3ab50da2a81b700de415d4a7a64d33
auzanassdq <auzanassidqi@gmail.com>
2021-05-08 16:06:21 +0700

    change Box w/ Container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, Container, theme } from '@chakra-ui/react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
@@ -9,40 +9,36 @@ import Login from './components/Login';
 import Navbar from './components/Navbar';
 import AdminDashboard from './components/AdminDashboard';
 
-function Home ({isLogin, userId}) {
-  if (isLogin) {
-    if (userId === "admin") {
-      return <Redirect to="/dashboard" />
-    }
-    return <ListMatkul />
-  }
-  return <Redirect to="/login" />
+function Home({ isLogin, userId }) {
+  if (isLogin)
+    return userId === 'admin' ? <Redirect to="/dashboard" /> : <ListMatkul />;
+  return <Redirect to="/login" />;
 }
 
 function App() {
   const { isLogin, userId } = useSelector(state => state.userReducer);
-  console.log(isLogin)
+  console.log(isLogin);
 
   return (
     <ChakraProvider theme={theme}>
-      <Box>
+      <Container maxW="container.xl">
         <Navbar />
 
-      <Switch>
-        <Route exact path="/">
-          <Home isLogin={isLogin} userId={userId} />
-        </Route>
-        <Route path="/login">
-          {isLogin ? <Redirect to="/" /> : <Login />}
-        </Route>
-        <Route path="/dashboard">
-          <AdminDashboard />
-        </Route>
-        <Route path="/matkul/:idMatkul">
-          <Matkul />
-        </Route>
-      </Switch>
-      </Box>
+        <Switch>
+          <Route exact path="/">
+            <Home isLogin={isLogin} userId={userId} />
+          </Route>
+          <Route path="/login">
+            {isLogin ? <Redirect to="/" /> : <Login />}
+          </Route>
+          <Route path="/dashboard">
+            <AdminDashboard />
+          </Route>
+          <Route path="/matkul/:idMatkul">
+            <Matkul />
+          </Route>
+        </Switch>
+      </Container>
     </ChakraProvider>
   );
 }
