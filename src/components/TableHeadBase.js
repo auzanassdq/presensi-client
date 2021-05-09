f711310d6bbba2607274d433abe685f29c788759
auzanassdq <auzanassidqi@gmail.com>
2021-05-09 22:13:10 +0700

    create table head base

---
+++ src/components/TableHeadBase.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Th, Thead, Tr, useColorModeValue } from '@chakra-ui/table';
+
+export default function TableHeadBase({ headers }) {
+  const thColor = useColorModeValue('blackAlpha.400', 'blackAlpha.700');
+
+  return (
+    <Thead bg={thColor}>
+      <Tr>
+        {headers.map((column, index) => (
+          <Th
+            key={index}
+            {...column.getHeaderProps(column.getSortByToggleProps())}
+          >
+            {column.render('Header')}
+            {column.isSorted ? (column.isSortedDesc ? ' ∨' : ' ∧') : ''}
+          </Th>
+        ))}
+      </Tr>
+    </Thead>
+  );
+}
