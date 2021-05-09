3c6be5d658075cfb5de35bbf483a751fd99bd4bd
auzanassdq <auzanassidqi@gmail.com>
2021-05-09 22:13:50 +0700

    refactor table base

--- src/components/TableBase.js
+++ src/components/TableBase.js
@@ -1,82 +1,15 @@
-import { useTable, useSortBy } from 'react-table';
 import React from 'react';
-import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/table';
-import { Button } from '@chakra-ui/button';
-import { useColorModeValue } from '@chakra-ui/color-mode';
-import { useHistory, useRouteMatch } from 'react-router';
+import { Table } from '@chakra-ui/react';
 
-export default function TableBase({ columns, data }) {
-  const thColor = useColorModeValue('blackAlpha.400', 'blackAlpha.700');
-  const history = useHistory();
-  const { path, url } = useRouteMatch();
-  console.log(path, url);
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headers,
-    rows,
-    prepareRow,
-  } = useTable(
-    {
-      columns,
-      data,
-    },
-    useSortBy
-  );
+import TableHeadBase from './TableHeadBase';
 
-  const firstPageRows = rows.slice(0, 20);
-
-  const lihatPertemuan = matkul => {
-    console.log(matkul);
-    history.push(`${url}/${matkul._id}`);
-  };
+export default function TableBase(props) {
+  const { headers, getTableProps } = props.tableHead;
 
   return (
     <Table variant="striped" colorScheme="blue" size="sm" {...getTableProps()}>
-      <Thead>
-        <Tr bg={thColor}>
-          {headers.map((column, index) => (
-            <Th
-              key={index}
-              {...column.getHeaderProps(column.getSortByToggleProps())}
-            >
-              {column.render('Header')}
-              {column.isSorted ? (column.isSortedDesc ? ' ∨' : ' ∧') : ''}
-            </Th>
-          ))}
-        </Tr>
-      </Thead>
-      <Tbody {...getTableBodyProps()}>
-        {firstPageRows.map((row, index) => {
-          prepareRow(row);
-          return (
-            <Tr key={index} {...row.getRowProps()}>
-              {row.cells.map((cell, index) => {
-                // console.log(cell);
-                if (cell.column.Header === 'Opsi') {
-                  return (
-                    <Td key={index} p="1">
-                      <Button
-                        colorScheme="blue"
-                        variant="outline"
-                        size="xs"
-                        onClick={() => lihatPertemuan(cell.row.original)}
-                      >
-                        Pertemuan
-                      </Button>
-                    </Td>
-                  );
-                }
-                return (
-                  <Td {...cell.getCellProps()} fontSize="xs">
-                    {cell.render('Cell')}
-                  </Td>
-                );
-              })}
-            </Tr>
-          );
-        })}
-      </Tbody>
+      <TableHeadBase headers={headers} />
+      {props.children}
     </Table>
   );
 }
