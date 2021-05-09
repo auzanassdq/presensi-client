46b1f6b83a34b95e2fcd936f70b52fab9f30a225
auzanassdq <auzanassidqi@gmail.com>
2021-05-09 22:16:11 +0700

    reafactor matkul table

--- src/components/MatkulTable.js
+++ src/components/MatkulTable.js
@@ -1,14 +1,27 @@
-import { Button } from '@chakra-ui/button';
-import { useDisclosure } from '@chakra-ui/hooks';
-import { Flex, Heading, Spacer } from '@chakra-ui/layout';
 import React, { useState } from 'react';
+import { useTable, useSortBy } from 'react-table';
+import { useHistory, useRouteMatch } from 'react-router';
 import { useSelector } from 'react-redux';
+import {
+  Button,
+  Flex,
+  Heading,
+  Spacer,
+  Tbody,
+  Tr,
+  Td,
+  useDisclosure,
+} from '@chakra-ui/react';
+
 import FormModal from './FormModal';
 import TableBase from './TableBase';
 
 export default function MatkulTable() {
+  const history = useHistory();
+  const { path, url } = useRouteMatch();
   const { allMatkul } = useSelector(state => state.matkulReducer);
   const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [columns] = useState([
     { Header: 'Kode', accessor: 'kode' },
     { Header: 'Nama', accessor: 'nama' },
@@ -17,6 +30,26 @@ export default function MatkulTable() {
     { Header: 'Semester', accessor: 'semester' },
     { Header: 'Opsi', accessor: 'opsi' },
   ]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headers,
+    rows,
+    prepareRow,
+  } = useTable(
+    {
+      columns,
+      data: allMatkul,
+    },
+    useSortBy
+  );
+  const firstPageRows = rows.slice(0, 20);
+
+  const lihatPertemuan = matkul => {
+    console.log(matkul);
+    history.push(`${url}/${matkul._id}`);
+  };
 
   return (
     <>
@@ -28,7 +61,40 @@ export default function MatkulTable() {
         </Button>
         <FormModal isOpen={isOpen} onClose={onClose} />
       </Flex>
-      <TableBase columns={columns} data={allMatkul} />
+
+      <TableBase tableHead={{ headers, getTableProps }}>
+        <Tbody {...getTableBodyProps()}>
+          {firstPageRows.map(row => {
+            prepareRow(row);
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  if (cell.column.Header === 'Opsi') {
+                    // console.log(cell.getCellProps());
+                    return (
+                      <Td {...cell.getCellProps()} p="1">
+                        <Button
+                          colorScheme="blue"
+                          variant="outline"
+                          size="xs"
+                          onClick={() => lihatPertemuan(cell.row.original)}
+                        >
+                          Pertemuan
+                        </Button>
+                      </Td>
+                    );
+                  }
+                  return (
+                    <Td {...cell.getCellProps()} fontSize="xs">
+                      {cell.render('Cell')}
+                    </Td>
+                  );
+                })}
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </TableBase>
     </>
   );
 }
