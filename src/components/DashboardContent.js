c78d4854dbe7d706cedcd60eabde20d6f69ee151
auzanassdq <auzanassidqi@gmail.com>
2021-05-09 22:10:50 +0700

    add route switch

--- src/components/DashboardContent.js
+++ src/components/DashboardContent.js
@@ -1,20 +1,23 @@
 import React, { useEffect } from 'react';
-import {
-  Box,
-  useColorModeValue,
-} from '@chakra-ui/react';
-import { useParams } from 'react-router';
+import { Box, Heading, useColorModeValue } from '@chakra-ui/react';
+import { Route, Switch, useParams, useRouteMatch } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllMatkul } from '../redux/actions/matkulAction';
-import MatkulTable from './MatkulTable';
 
-export default function DashboardContent() {
+import { getAllMatkul } from '../redux/actions/matkulAction';
+
+import MatkulTable from './MatkulTable';
+import PertemuanTable from './PertemuanTable';
+
+export default function DashboardContent({ dashboardURL }) {
   const { content } = useParams();
+  let { path, url } = useRouteMatch();
+  // console.log(content);
+  // console.log(path, url);
+
   const dispatch = useDispatch();
   const boxGray = useColorModeValue('gray.200', 'gray.700');
   const { allMatkul, matkul } = useSelector(state => state.matkulReducer);
 
-  console.log(allMatkul);
   useEffect(() => {
     switch (content) {
       case 'dosen':
@@ -30,7 +33,24 @@ export default function DashboardContent() {
 
   return (
     <Box rounded={5} bg={boxGray} h="600px" w="100%" ml="0px">
-      <MatkulTable />
+      <Switch>
+        <Route exact path={`${path}`}>
+          <ContentSwitcher content={content} />
+        </Route>
+        <Route path={`${dashboardURL}/matkul/:matkulId`}>
+          <PertemuanTable />
+        </Route>
+        <Heading>Ngaco</Heading>
+      </Switch>
     </Box>
   );
+}
+
+function ContentSwitcher({ content }) {
+  switch (content) {
+    case 'matkul':
+      return <MatkulTable />;
+    case 'dosen':
+      return <Heading>Konten Dosen</Heading>;
+  }
 }
