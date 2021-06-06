7a69094ec0e94184ffa9342aad3a2a1c7810f414
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:58:14 +0700

    add logic for attendance when it's time

--- src/components/PertemuanItem.js
+++ src/components/PertemuanItem.js
@@ -1,4 +1,4 @@
-import { useColorModeValue } from '@chakra-ui/color-mode';
+import moment from 'moment';
 import {
   Center,
   Circle,
@@ -8,27 +8,45 @@ import {
   ListItem,
   Spacer,
   Text,
-} from '@chakra-ui/layout';
-import { CheckIcon, CloseIcon } from '@chakra-ui/icons';
-import { useDisclosure } from '@chakra-ui/hooks';
+  useDisclosure,
+  useColorModeValue,
+  useToast,
+} from '@chakra-ui/react';
+import { CheckIcon, CloseIcon, MinusIcon } from '@chakra-ui/icons';
 
-import Modal from './ScanModal';
-import moment from 'moment';
+import ScanModal from './ScanModal';
+import Color from '../utilities/Color';
 
 export default function PertemuanItem({ pertemuan, lastItem }) {
-  const fromBg = useColorModeValue('gray.200', 'gray.700');
-
-  const checkColor = useColorModeValue('blue.100', 'blue.900');
-  const textCheckColor = useColorModeValue('blue.500', 'blue.500');
-
-  const unCheckColor = useColorModeValue('pink.100', 'pink.900');
-  const textUnCheckColor = useColorModeValue('pink.500', 'pink.500');
+  const toast = useToast();
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const handleMatkul = item => {
-    console.log(item);
-    onOpen();
+    let jadwalPertemuan = new Date(item.jadwal).getTime();
+    let batasHadir = jadwalPertemuan + 1800000;
+
+    if (Date.now() < jadwalPertemuan) {
+      console.log('belum waktunya');
+      toast({
+        title: 'Pertemuan belum dimulai',
+        status: 'info',
+        duration: 2000,
+        isClosable: true,
+      });
+    } else if (Date.now() >= jadwalPertemuan && Date.now() <= batasHadir) {
+      console.log('silahkan');
+      onOpen();
+    } else {
+      console.log('dah lewat');
+      toast({
+        title: 'Pertemuan sudah berlalu',
+        status: 'error',
+        duration: 2000,
+        isClosable: true,
+      });
+    }
+    // onOpen();
   };
 
   return (
@@ -38,19 +56,26 @@ export default function PertemuanItem({ pertemuan, lastItem }) {
       overflow="hidden"
       onClick={() => handleMatkul(pertemuan)}
     >
-      <Modal isOpen={isOpen} onClose={onClose} pertemuan={pertemuan} />
+      <ScanModal isOpen={isOpen} onClose={onClose} pertemuan={pertemuan} />
       <Flex direction="row">
         <Flex direction="column" spacing="0">
-          <Circle
-            size="40px"
-            bg={pertemuan.kehadiran.status ? checkColor : unCheckColor}
-          >
-            {pertemuan.kehadiran.status ? (
-              <CheckIcon color={textCheckColor} />
-            ) : (
-              <CloseIcon color={textUnCheckColor} />
-            )}
-          </Circle>
+          {new Date(pertemuan.jadwal).getTime() > Date.now() ? (
+            <Circle size="40px" bg={Color().netralColor}>
+              <MinusIcon color={Color().textNetralColor} />
+            </Circle>
+          ) : (
+            <Circle
+              size="40px"
+              bg={pertemuan.kehadiran.status ? Color().checkColor : Color().unCheckColor}
+            >
+              {pertemuan.kehadiran.status ? (
+                <CheckIcon color={Color().textCheckColor} />
+              ) : (
+                <CloseIcon color={Color().textUnCheckColor} />
+              )}
+            </Circle>
+          )}
+
           {!lastItem && (
             <Center height="100px" size="10px">
               <Divider orientation="vertical" />
@@ -61,12 +86,17 @@ export default function PertemuanItem({ pertemuan, lastItem }) {
         <Flex direction="column" w="260px">
           <Heading size="sm">{pertemuan.nama}</Heading>
           <Text fontSize="xs" color="gray.400">
-            {moment(pertemuan.kehadiran.checkIn).format(
-              'DD MMMM YYYY, kk:mm:ss'
-            )}
-            {/* {Date(pertemuan.kehadiran.checkIn)} */}
+            {moment(pertemuan.jadwal).format('dddd, DD MMMM YYYY, kk:mm')}
           </Text>
-          <Flex rounded={5} h="50px" bg={fromBg} mt="10px"></Flex>
+          <Flex rounded={5} bg={Color().cardBg} mt="10px" p="2" justifyContent="center">
+            {/* {pertemuan.kehadiran.checkIn < pertemuan.jadwal
+              ? '-'
+              : moment(pertemuan.kehadiran.checkIn).format('kk:mm')} */}
+
+            {pertemuan.kehadiran.status
+              ? moment(pertemuan.kehadiran.checkIn).format('kk:mm')
+              : '-'}
+          </Flex>
         </Flex>
       </Flex>
     </ListItem>
