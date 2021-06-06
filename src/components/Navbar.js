3d59208e9b26844948d6b0c1f71630983606dc17
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:38:34 +0700

    add menu drop down for logout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,28 @@
-import { Heading, Flex, Spacer, Box, Center, Button } from '@chakra-ui/react';
+import { ExternalLinkIcon, HamburgerIcon } from '@chakra-ui/icons';
+import {
+  Heading,
+  Flex,
+  Spacer,
+  Box,
+  Center,
+  Button,
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from '@chakra-ui/react';
 import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router';
+
 import { ColorModeSwitcher } from '../ColorModeSwitcher';
 import { logout } from '../redux/actions/userAction';
 
 function Navbar() {
   const dispatch = useDispatch();
-  const { isLogin, userId } = useSelector(state => state.userReducer);
+  const history = useHistory();
+  const { isLogin, userId, nama } = useSelector(state => state.userReducer);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -14,20 +31,42 @@ function Navbar() {
   return (
     <Flex pt="4" pb="4" alignItems="center">
       <Center>
-        <Heading size="lg">PRESENSI</Heading>
+        <Heading size="lg" onClick={() => history.push('/')}>
+          PRESENSI
+        </Heading>
       </Center>
       <Spacer />
       {isLogin && userId != 'admin' && (
-        <Button variant="link" size="sm" mr="2" onClick={handleLogout}>
+        <Button
+          variant="link"
+          size="sm"
+          mr="2"
+          onClick={() => history.push('/matkul')}
+        >
           Daftar Matkul
         </Button>
       )}
       {/* <Box> */}
       <ColorModeSwitcher />
       {isLogin && (
-        <Button variant="outline" size="sm" ml="2" onClick={handleLogout}>
-          Keluar
-        </Button>
+        // <Button borderRadius="full" variant="outline" size="sm" ml="2" onClick={handleLogout}>
+        //   Keluar
+        // </Button>
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<HamburgerIcon />}
+            variant="outline"
+          />
+          <MenuList>
+            <MenuItem>{nama}</MenuItem>
+            <MenuDivider />
+            <MenuItem icon={<ExternalLinkIcon />} onClick={handleLogout}>
+              Keluar
+            </MenuItem>
+          </MenuList>
+        </Menu>
       )}
       {/* </Box> */}
     </Flex>
