9eb14d2926f53ef608091eb71cc4b30b68bde4ff
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:54:39 +0700

    add unenroll matkul

--- src/components/Matkul.js
+++ src/components/Matkul.js
@@ -1,20 +1,40 @@
+import { Button } from '@chakra-ui/button';
 import { Center } from '@chakra-ui/layout';
 import { Box, Heading } from '@chakra-ui/layout';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 
-import { getMatkulByID } from '../redux/actions/matkulAction';
+import { getMatkulByID, removeAmbilKelas } from '../redux/actions/matkulAction';
+
 import ListPertemuan from './ListPertemuan';
 
 function Matkul() {
   const { idMatkul } = useParams();
   const dispatch = useDispatch();
+  const history = useHistory()
   const { matkul } = useSelector(state => state.matkulReducer);
+  const { userId } = useSelector(state => state.userReducer);
+
 
   useEffect(() => {
     dispatch(getMatkulByID(idMatkul));
   }, [dispatch, idMatkul]);
+
+  const handleUnenrollMatkul = () => {
+    let data = {
+      matkul: idMatkul,
+      mahasiswa: userId
+    }
+    
+    dispatch(removeAmbilKelas(data))
+    .then(() => {
+      history.push("/")
+    })
+    .catch(() => {
+      console.log("gagal");
+    })
+  }
 
   return (
     <Box p={2}>
@@ -23,6 +43,9 @@ function Matkul() {
           <Heading textAlign="center">{matkul.nama}</Heading>
           <Center>
             <ListPertemuan matkulId={idMatkul} />
+          </Center>
+          <Center>
+          <Button variant="link" mt="100px" onClick={handleUnenrollMatkul}>Unenroll</Button>
           </Center>
         </>
       )}
