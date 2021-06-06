07d2642063ab6623ec37fb4c184c5cd75d6f5730
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:35:59 +0700

    remove useEffect, create route for dosen and mahasiswa

--- src/components/admin/DashboardContent.js
+++ src/components/admin/DashboardContent.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
 import { Box, Heading, useColorModeValue } from '@chakra-ui/react';
 import { Route, Switch, useParams, useRouteMatch } from 'react-router';
 import { useDispatch } from 'react-redux';
 
 import MatkulTable from './matkul/MatkulTable';
 import PertemuanTable from './pertemuan/PertemuanTable';
+import DosenTable from './dosen/DosenTable';
+import MahasiswaTable from './mahasiswa/MahasiswaTable';
 
 export default function DashboardContent({ dashboardURL }) {
   const { content } = useParams();
@@ -13,21 +14,8 @@ export default function DashboardContent({ dashboardURL }) {
   const dispatch = useDispatch();
   const boxGray = useColorModeValue('gray.200', 'gray.700');
 
-  useEffect(() => {
-    switch (content) {
-      case 'dosen':
-        // dispatch(getDosen())
-        break;
-      case 'matkul':
-        
-        break;
-      default:
-        break;
-    }
-  }, [dispatch, content]);
-
   return (
-    <Box rounded={5} bg={boxGray} h="600px" w="100%" ml="0px">
+    <Box rounded={5} bg={boxGray} h="100%" w="100%" ml="0px">
       <Switch>
         <Route exact path={`${path}`}>
           <ContentSwitcher content={content} />
@@ -46,6 +34,8 @@ function ContentSwitcher({ content }) {
     case 'matkul':
       return <MatkulTable />;
     case 'dosen':
-      return <Heading>Konten Dosen</Heading>;
+      return <DosenTable />;
+    case 'mahasiswa':
+      return <MahasiswaTable />;
   }
 }
