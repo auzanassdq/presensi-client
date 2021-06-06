d5eeadd397781ddf3c89058ea1eed8371de69715
auzanassdq <auzanassidqi@gmail.com>
2021-06-06 13:30:09 +0700

    set reducer for dosen and mahasiswa

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,17 @@ import userReducer from './userReducer';
 import matkulReducer from './matkulReducer';
 import pertemuanReducer from './pertemuanReducer';
 import kehadiranReducer from './kehadiranReducer';
+import dosenReducer from './dosenReducer';
+import mahasiswaReducer from './mahasiswaReducer';
+
 
 const rootReducer = combineReducers({
   userReducer,
   matkulReducer,
   pertemuanReducer,
   kehadiranReducer,
+  dosenReducer,
+  mahasiswaReducer,
 });
 
 export default rootReducer;
