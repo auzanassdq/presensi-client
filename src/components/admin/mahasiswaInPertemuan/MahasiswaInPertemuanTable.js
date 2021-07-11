e5b6bc618c406d57e41c858c12c170ae93c6c2f4
auzanassdq <auzanassidqi@gmail.com>
2021-07-11 18:01:48 +0700

    hanndle null

--- src/components/admin/mahasiswaInPertemuan/MahasiswaInPertemuanTable.js
+++ src/components/admin/mahasiswaInPertemuan/MahasiswaInPertemuanTable.js
@@ -49,7 +49,7 @@ export default function MahasiswaInPertemuanTable() {
     useTable(
       {
         columns,
-        data: kehadiranByPertemuan,
+        data: kehadiranByPertemuan || [],
       },
       useSortBy
     );
